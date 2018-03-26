f43cdad4b0cf84725528afa044eca5b2fff3550b
Robin Wieruch <wrobin@gmx.net>
2018-03-26 13:23:05 +0800

    simplify state -> view

--- src/Users.js
+++ src/Users.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const createState = (type, data) => {
-  return {
-    fold: definitions => {
-      const renderFn = definitions[type];
-      return renderFn ? renderFn(data) : null;
-    },
-  };
-};
+const createState = (type, data) => ({
+  view: definitions => {
+    const renderFn = definitions[type];
+    return renderFn ? renderFn(data) : null;
+  },
+});
 
 const STATES = {
   init: () => createState('NOT_LOADED'),
@@ -16,17 +14,16 @@ const STATES = {
   success: data => createState('SUCCESS', data),
 };
 
-const fakeFetch = () => {
-  return new Promise(res => {
-    setTimeout(() => res([{ id: 1, name: 'foo' }]), 1000);
+const fakeFetch = () =>
+  new Promise(resolve => {
+    setTimeout(
+      () => resolve([{ id: 1, name: 'Robin' }, { id: 2, name: 'Dustin' }]),
+      1500,
+    );
   });
-};
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...STATES.init() };
-  }
+  state = { ...STATES.init() };
 
   componentDidMount() {
     this.fetch();
@@ -41,22 +38,16 @@ class Users extends React.Component {
   };
 
   render() {
-    const { fold } = this.state;
-
     return (
       <div>
         <h3>User List</h3>
 
-        {fold({
+        {this.state.view({
           NOT_LOADED: () => <div>Not Loaded</div>,
           LOADING: () => <div>Loading...</div>,
           FAILURE: error => <div>An error has happened</div>,
           SUCCESS: users => (
-            <ul>
-              {users.map(user => (
-                <li key={user.id}>{user.name}</li>
-              ))}
-            </ul>
+            <ul>{users.map(user => <li key={user.id}>{user.name}</li>)}</ul>
           ),
         })}
       </div>
